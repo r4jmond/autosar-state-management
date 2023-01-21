86a9b5b2f8c4619976e43c4ba54da8b16873b276
Mateusz Garwol <mgarwol@student.agh.edu.pl>
2023-01-21 20:40:48 +0100

    Add support for remaining FunctionGroups

--- tests/test_state_client.cpp
+++ tests/test_state_client.cpp
@@ -6,10 +6,22 @@ namespace ara { namespace exec {
     /* Mock StateClient for unit testing. */
     class MockStateClient {
     public:
+        MOCK_METHOD(void, CoreSetState, (sm::FunctionGroupStateType));
         MOCK_METHOD(void, ComSetState, (sm::FunctionGroupStateType));
+        MOCK_METHOD(void, CryptoSetState, (sm::FunctionGroupStateType));
         MOCK_METHOD(void, DiagSetState, (sm::FunctionGroupStateType));
-        MOCK_METHOD(void, PhmSetState, (sm::FunctionGroupStateType));
         MOCK_METHOD(void, ExecSetState, (sm::FunctionGroupStateType));
+        MOCK_METHOD(void, FwSetState, (sm::FunctionGroupStateType));
+        MOCK_METHOD(void, IamSetState, (sm::FunctionGroupStateType));
+        MOCK_METHOD(void, IdsmSetState, (sm::FunctionGroupStateType));
+        MOCK_METHOD(void, LogSetState, (sm::FunctionGroupStateType));
+        MOCK_METHOD(void, NmSetState, (sm::FunctionGroupStateType));
+        MOCK_METHOD(void, NaSetState, (sm::FunctionGroupStateType));
+        MOCK_METHOD(void, PerSetState, (sm::FunctionGroupStateType));
+        MOCK_METHOD(void, PhmSetState, (sm::FunctionGroupStateType));
+        MOCK_METHOD(void, SmSetState, (sm::FunctionGroupStateType));
+        MOCK_METHOD(void, TsyncSetState, (sm::FunctionGroupStateType));
+        MOCK_METHOD(void, UcmSetState, (sm::FunctionGroupStateType));
     };
 }}
 
